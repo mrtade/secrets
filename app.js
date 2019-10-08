bf887208eb0f582926a84185e59a5d251d8472e8
mrtade <mrtade@users.noreply.github.com>
2019-10-08 20:45:20 +1000

    Add bcrypt hash and 10 rounds of salt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 // const encrypt = require('mongoose-encryption');
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 // express to use EJS
 app.set('view engine', 'ejs');
@@ -61,24 +63,30 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
 
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  bcrypt.hash(req.body.password, 10, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+
   });
 
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+
 });
 
 app.post("/login", function(req, res){
 
   const username = req.body.username;
-  const password = md5(req.body.password);
+  // const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
 
@@ -86,9 +94,12 @@ app.post("/login", function(req, res){
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true){
+            res.render("secrets");
+          }
+
+        });
       }
     }
   });
